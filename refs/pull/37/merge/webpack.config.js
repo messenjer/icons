95dc0845f5d0bfb8fd001792ebbe7d6685d4899b
daniarsuprianto <daniarsuprianto@users.noreply.github.com>
2020-12-16 15:20:24 +0000

    deploy: ad9e247072dcb6a6f79761a1d58079c821de6b1c

--- refs/pull/37/merge/webpack.config.js
+++ refs/pull/37/merge/webpack.config.js
@@ -1,12 +1,14 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development",
-  stats: "errors-only",
   entry: "./src/index.js",
   output: {
     filename: "index.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
+  },
+  devServer: {
+    contentBase: "./src",
+    hot: true,
+    stats: "errors-only",
   },
 };
