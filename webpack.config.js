e88f1b835eaf839a7c5cd61edf696b9859f35bbb
Daniar Suprianto <daniar@funda.nl>
2020-12-17 11:36:28 +0100

    fix(preview): add webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -7,7 +9,19 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
   devServer: {
+    contentBase: "./dist",
     hot: true,
     stats: "errors-only",
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "./src/**/*.(html|css)",
+          to: "[name].[ext]",
+        },
+      ],
+    }),
+  ],
 };
