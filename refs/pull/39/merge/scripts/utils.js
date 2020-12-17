97c4f870fedffd6e462aa66f1d113f762dd814e4
daniarsuprianto <daniarsuprianto@users.noreply.github.com>
2020-12-17 06:48:29 +0000

    deploy: d9c36ffa57f9c8c677f3d9217a61b31ccefe82ab

--- refs/pull/39/merge/scripts/utils.js
+++
@@ -1,9 +0,0 @@
-const { camelCase } = require("camel-case");
-
-module.exports = {
-  /**
-   * Converts a filename to title
-   * @param {String} filename The filename to convert (without full path)
-   */
-  filenameToTitle: (filename) => camelCase(filename.replace(/\.[^.]+$/gi, "")),
-};
